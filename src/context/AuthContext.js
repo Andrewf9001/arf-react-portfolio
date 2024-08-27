acaad78963761f809ba64dc128b67f3ae5d8313b
Andrewf9001 <andrewf9001@gmail.com>
2024-08-27 17:02:10 -0600

    created auth context, and provider

---
+++ src/context/AuthContext.js
@@ -0,0 +1,47 @@
+import { createContext, useContext, useState } from "react";
+import { auth } from "../services/firebase";
+import {
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const signIn = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const logout = () => {
+    return signOut(auth);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+      setIsLoading(false);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  const values = {
+    currentUser,
+    signIn,
+    logout,
+  };
+
+  return (
+    <AuthContext.Provider value={values}>
+      {!isLoading && children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
