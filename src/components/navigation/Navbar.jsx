85639548a62a4297289c370011a820b1e582dfa5
Andrewf9001 <andrewf9001@gmail.com>
2024-09-05 17:14:20 -0600

    styled navbar

--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -9,17 +9,12 @@ const Navbar = () => {
       <div className="left-column">
         <NavLink to="/">Home</NavLink>
         <NavLink to="/about">About</NavLink>
+        <NavLink to="/dashboard">Dashboard</NavLink>
       </div>
 
       {currentUser && (
         <div className="right-column">
-          <div className="left-column-wrapper">
-            <NavLink to="/dashboard">Dashboard</NavLink>
-          </div>
-
-          <div className="right-column-wrapper">
-            <button onClick={logout}>Logout</button>
-          </div>
+          <button onClick={logout}>Logout</button>
         </div>
       )}
     </div>
