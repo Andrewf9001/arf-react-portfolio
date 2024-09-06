f7bb76cbf351eda420dd5a6ec1f33c59fda03422
Andrewf9001 <andrewf9001@gmail.com>
2024-09-06 14:42:25 -0600

    added basic structure to about page

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,7 +1,32 @@
+import profilePhoto from "../../assets/images/personal-photo.jpeg";
+
 const About = () => {
   return (
     <div className="about-container">
-      <h1>About</h1>
+      <img src={profilePhoto} alt="Andrew Fletcher Headshot" />
+
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium
+        eligendi omnis explicabo natus pariatur laudantium velit ad ab odio at
+        iusto modi esse ex, dolores a sed reprehenderit labore repellat?
+        Repellendus aspernatur voluptas nihil non placeat soluta rerum ipsam
+        eaque tempora provident magnam et facilis, doloremque minima eos
+        reprehenderit dolorem quasi aliquam facere cumque, in qui saepe nam
+        modi? Accusamus! Sequi, blanditiis molestiae? Ipsa labore animi repellat
+        porro commodi excepturi blanditiis laborum fugit cum, corrupti illo
+        ducimus esse minima molestias reiciendis facere! Maxime quisquam
+        assumenda, fuga optio illum consectetur omnis? Odio nisi atque saepe
+        consequuntur iusto suscipit culpa pariatur error unde, omnis, quo iste
+        nulla vitae harum. Libero, delectus quaerat nemo ratione architecto quam
+        fugit natus numquam, error id nulla! Repudiandae natus ipsum voluptas
+        voluptates accusamus harum. Labore repellendus harum rerum, quis
+        voluptate consectetur numquam quidem ex. Natus, magni dolorum, minus
+        eligendi, enim ratione laudantium sit quo a consequuntur ducimus. Quo
+        quas sunt aspernatur veritatis facilis. Accusantium ducimus impedit
+        similique dolores magni vitae assumenda, maxime alias. Autem alias
+        aliquid earum illo a pariatur similique odio natus minus, aspernatur
+        maxime ea!
+      </p>
     </div>
   );
 };
