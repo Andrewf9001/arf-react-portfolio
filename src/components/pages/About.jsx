11813f480bdca1c03b1ce120cb936cb0ea6b2732
Andrewf9001 <andrewf9001@gmail.com>
2024-09-06 14:53:10 -0600

    added basic styles to about page

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -3,30 +3,62 @@ import profilePhoto from "../../assets/images/personal-photo.jpeg";
 const About = () => {
   return (
     <div className="about-container">
-      <img src={profilePhoto} alt="Andrew Fletcher Headshot" />
+      <div className="content-wrapper">
+        <img src={profilePhoto} alt="Andrew Fletcher Headshot" />
 
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium
-        eligendi omnis explicabo natus pariatur laudantium velit ad ab odio at
-        iusto modi esse ex, dolores a sed reprehenderit labore repellat?
-        Repellendus aspernatur voluptas nihil non placeat soluta rerum ipsam
-        eaque tempora provident magnam et facilis, doloremque minima eos
-        reprehenderit dolorem quasi aliquam facere cumque, in qui saepe nam
-        modi? Accusamus! Sequi, blanditiis molestiae? Ipsa labore animi repellat
-        porro commodi excepturi blanditiis laborum fugit cum, corrupti illo
-        ducimus esse minima molestias reiciendis facere! Maxime quisquam
-        assumenda, fuga optio illum consectetur omnis? Odio nisi atque saepe
-        consequuntur iusto suscipit culpa pariatur error unde, omnis, quo iste
-        nulla vitae harum. Libero, delectus quaerat nemo ratione architecto quam
-        fugit natus numquam, error id nulla! Repudiandae natus ipsum voluptas
-        voluptates accusamus harum. Labore repellendus harum rerum, quis
-        voluptate consectetur numquam quidem ex. Natus, magni dolorum, minus
-        eligendi, enim ratione laudantium sit quo a consequuntur ducimus. Quo
-        quas sunt aspernatur veritatis facilis. Accusantium ducimus impedit
-        similique dolores magni vitae assumenda, maxime alias. Autem alias
-        aliquid earum illo a pariatur similique odio natus minus, aspernatur
-        maxime ea!
-      </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis
+          reiciendis, nam atque laboriosam illo repellat sit dolorem tenetur
+          beatae ipsam, sint facere. Cupiditate minus nam aperiam nostrum, eos
+          dolorem eum. In explicabo optio id asperiores corporis magni, nemo
+          eligendi, inventore tenetur, at quibusdam libero quasi esse numquam
+          unde quae similique quaerat sequi! Nam fugit laborum aliquid soluta
+          repellat ad mollitia! Accusantium officiis saepe exercitationem
+          tempora fuga eos nam, assumenda quibusdam doloribus obcaecati quis.
+          Corrupti velit adipisci dignissimos, facilis omnis magni distinctio
+          facere nulla explicabo delectus libero ducimus laboriosam culpa
+          maiores. Sapiente culpa cumque, tempora qui, quidem alias harum
+          accusantium quos omnis expedita magni numquam reprehenderit inventore
+          cupiditate earum iure sunt vel ducimus excepturi sed voluptatibus,
+          pariatur quia fugit illo. Deleniti? Quae minima sint magnam quam quasi
+          eius facere, molestiae quidem rem ut atque cum provident odit id
+          commodi nihil et corporis, aliquam totam ea impedit quia sunt magni
+          ex. Autem. Labore, maiores illo deleniti quod rem dicta quis
+          quibusdam. Nisi beatae deserunt dolorum tempore consequatur aperiam
+          nulla vel quaerat ad eius deleniti, architecto quae reiciendis modi
+          nemo ipsam, soluta quam? Deleniti eveniet ullam natus explicabo autem
+          necessitatibus, harum repellat magni, recusandae vero saepe voluptas
+          dignissimos repellendus distinctio sed quidem dolorem alias fuga?
+          Aliquid, reiciendis? Sapiente, illo soluta! Eligendi, quam
+          dignissimos. Nemo architecto alias minima perferendis. Mollitia
+          commodi ipsam vero vel dignissimos sequi blanditiis nostrum? Veritatis
+          hic ad eaque et nemo, nulla, eligendi voluptatum molestias
+          voluptatibus saepe nam debitis fuga eius. Reiciendis sapiente incidunt
+          illo, odio modi sit ipsa repellendus sint praesentium eius nobis
+          dignissimos id ea eum doloribus facilis aperiam voluptatibus officia
+          non totam at ipsam maiores accusamus. Ea, id. Autem cum molestias
+          aliquid at recusandae commodi illo obcaecati omnis, dolore sunt non
+          soluta. Minima laborum, aut blanditiis culpa illo ullam ut, vel
+          tempora quae reprehenderit nihil, molestias neque sed. Illo iusto
+          rerum reiciendis velit ut modi, libero hic ipsa qui! Cupiditate
+          nesciunt accusamus, exercitationem natus quasi veritatis eius,
+          assumenda ducimus itaque in voluptas quos iste mollitia vero? Ipsum,
+          necessitatibus? Assumenda, dicta perferendis eveniet sapiente
+          voluptatem sint omnis rem numquam, magnam sit laboriosam nesciunt
+          praesentium fugit alias odit iusto, cum eligendi voluptates maiores.
+          Recusandae, minus hic unde explicabo natus dolores. Neque facilis,
+          libero qui tempora harum aut similique suscipit illo a reprehenderit
+          molestiae, sit dolore temporibus, saepe sunt fugit. Sapiente aliquid
+          ratione earum minus repellendus voluptatibus incidunt porro provident
+          natus! Ad, possimus eos laudantium doloribus dicta ut alias
+          consequatur veritatis beatae! Asperiores ad, voluptatibus suscipit et
+          possimus quo, voluptates dolorem commodi unde autem architecto quis
+          blanditiis placeat sapiente tenetur officia? Eum optio expedita nihil
+          consequuntur nulla dicta, quos, cum fugit neque animi voluptatibus in
+          et officiis, iure laborum modi adipisci assumenda enim quaerat
+          veritatis ex corrupti pariatur odio. Tempore, earum.
+        </p>
+      </div>
     </div>
   );
 };
