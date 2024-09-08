6c82f916af5b5c2f43e150274d5f7ccc4a0ded6b
Andrewf9001 <andrewf9001@gmail.com>
2024-09-08 09:48:28 -0600

    added dashboard todos

--- src/components/pages/Dashboard.jsx
+++ src/components/pages/Dashboard.jsx
@@ -1,3 +1,15 @@
+/* 
+  TODO:
+    - What can I do in the dashboard?
+      - Navigate to form section to add new item
+        - Rich text editor
+        - Image uploads
+        - Video uploads
+        - Thumbnail, Banner, Images
+      - Activate / Deactivate which projects to show
+      - Update the About page (or any page with details) possibly
+*/
+
 const Dashboard = () => {
   return (
     <div className="dashboard-container">
