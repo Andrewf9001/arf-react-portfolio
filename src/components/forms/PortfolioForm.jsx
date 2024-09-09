21cbd098f829568457231f256efdd371803630e3
Andrewf9001 <andrewf9001@gmail.com>
2024-09-09 16:13:18 -0600

    added base portfolio form

---
+++ src/components/forms/PortfolioForm.jsx
@@ -0,0 +1,9 @@
+const PortfolioForm = () => {
+  return (
+    <form className="portfolio-form">
+      <h1>Portfolio Form</h1>
+    </form>
+  );
+};
+
+export default PortfolioForm;
--- src/components/pages/PortfiolioManager.jsx
+++ src/components/pages/PortfiolioManager.jsx
@@ -13,3 +13,15 @@ const PortfolioManager = () => {
 };
 
 export default PortfolioManager;
+
+/* 
+  TODO:
+    - What can I do in the dashboard?
+      - Navigate to form section to add new item
+        - Rich text editor
+        - Image uploads
+        - Video uploads
+        - Thumbnail, Banner, Images
+      - Activate / Deactivate which projects to show
+      - Update the About page (or any page with details) possibly
+*/
