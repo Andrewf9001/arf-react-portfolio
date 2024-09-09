049f3472e639cf1556a51180b1d201789557d52d
Andrewf9001 <andrewf9001@gmail.com>
2024-09-09 16:23:07 -0600

    added portfolio side + features

---
+++ src/components/features/PortfolioSidebar.jsx
@@ -0,0 +1,9 @@
+const PortfolioSidebar = () => {
+  return (
+    <div className="portfolio-sidebar-container">
+      <h1>Portfolio Sidebar</h1>
+    </div>
+  );
+};
+
+export default PortfolioSidebar;
