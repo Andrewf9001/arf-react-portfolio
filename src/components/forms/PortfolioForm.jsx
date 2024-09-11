8efcb74c15d78ea308e055a700f860f6dad9f535
Andrewf9001 <andrewf9001@gmail.com>
2024-09-11 14:49:21 -0600

    added basic structure to portfolio form

--- src/components/forms/PortfolioForm.jsx
+++ src/components/forms/PortfolioForm.jsx
@@ -1,7 +1,36 @@
 const PortfolioForm = () => {
   return (
     <form className="portfolio-form-container">
-      <h1>Portfolio Form</h1>
+      <div className="two-column">
+        <input type="text" /> {/* name */}
+        <select>
+          <option>Hobbies</option>
+          <option>Web Development</option>
+        </select>
+      </div>
+
+      <div className="two-column">
+        <input type="text" /> {/* url */}
+        <input type="text" /> {/* url display text */}
+      </div>
+
+      <div className="one-column">
+        <input type="text" /> {/* description */}
+      </div>
+
+      <div className="upload-wrapper">
+        <div className="image-wrapper">
+          <h1>Thumbnail Image/Upload</h1>
+        </div>
+
+        <div className="image-wrapper">
+          <h1>Banner Image/Upload</h1>
+        </div>
+
+        <div className="image-wrapper">
+          <h1>Logo Image/Upload</h1>
+        </div>
+      </div>
     </form>
   );
 };
