8c339123b9b081e29ad1886a97e09e06e7c5fdaf
Andrewf9001 <andrewf9001@gmail.com>
2024-09-12 16:14:58 -0600

    mapped data in portfolio sidebar

--- src/components/features/PortfolioSidebar.jsx
+++ src/components/features/PortfolioSidebar.jsx
@@ -1,7 +1,35 @@
+import { useAppData } from "../../context/AppDataContext";
+
 const PortfolioSidebar = () => {
+  const { hobbies, projects } = useAppData();
+
+  const renderData = (data) => {
+    return data.map((item) => {
+      return (
+        <div className="portfolio-item-wrapper">
+          <img
+            src={item.thumbUrl}
+            alt={`${item.name} thumbnail`}
+            className="thumb-image"
+          />
+
+          <div className="text-content">
+            <div className="name">{item.name}</div>
+
+            <div className="buttons-wrapper">
+              <button className="edit">Edit Fontawesome Icon</button>
+              <button className="delete">Trash Fontawesome Icon</button>
+            </div>
+          </div>
+        </div>
+      );
+    });
+  };
+
   return (
     <div className="portfolio-sidebar-container">
-      <h1>Portfolio Sidebar</h1>
+      {renderData(hobbies)}
+      {renderData(projects)}
     </div>
   );
 };
