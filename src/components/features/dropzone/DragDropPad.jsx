c164f95c4fe5fb85e7f36b66104f3226b339a3ae
Andrewf9001 <andrewf9001@gmail.com>
2024-09-16 16:38:58 -0600

    created todo for dropzone to mesh with firebase + refs

--- src/components/features/dropzone/DragDropPad.jsx
+++ src/components/features/dropzone/DragDropPad.jsx
@@ -1,8 +1,27 @@
 import { forwardRef } from "react";
 
 const DragDropPad = forwardRef(({ children, accept }, ref) => {
+  const handleOnDrop = () => {
+    e.preventDefault();
+
+    const droppedFiles =
+      e.target.type === "file" ? e.target.files : e.dataTransfer.files;
+
+    if (droppedFiles) {
+      ref.current;
+    }
+  };
+
+  const handleOnDragOver = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div className="drag-drop-container">
+    <div
+      className="drag-drop-container"
+      onDragOver={handleOnDragOver}
+      onDrop={handleOnDrop}
+    >
       <input ref={ref} type="file" accept={accept} />
 
       {children}
@@ -11,3 +30,10 @@ const DragDropPad = forwardRef(({ children, accept }, ref) => {
 });
 
 export default DragDropPad;
+
+/* 
+  TODO:
+    - build onSuccessfulDrop function
+    - Identify how to set the images to the refs
+    - create previews when an image is selected
+*/
