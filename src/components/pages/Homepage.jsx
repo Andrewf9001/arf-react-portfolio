ffbd723f0920cfb340ed9475a019b8575cb69581
Andrewf9001 <andrewf9001@gmail.com>
2024-09-29 11:31:46 -0600

    added hover for homepage, added layout styles

--- src/components/pages/Homepage.jsx
+++ src/components/pages/Homepage.jsx
@@ -1,22 +1,24 @@
 import { Link } from "react-router-dom";
 import { useAppData } from "../../context/AppDataContext";
-import { useState } from "react";
 
 const Homepage = () => {
-  const [itemClassName, setItemClassName] = useState("");
   const { hobbies, projects } = useAppData();
 
   const renderData = (data) => {
     return data?.map((item) => {
       return (
-        <Link className="item-wrapper" to={`/project/${item.id}`}>
-          <div
-            className="portfolio-item-wrapper"
-            onMouseEnter={() => setItemClassName("image-blur")}
-            onMouseLeave={() => setItemClassName("")}
-          >
-            <img src={item.thumbUrl} alt={`${item.name} Thumbnail`} />
-            <img src={item.thumbUrl} alt={`${item.name} Logo`} />
+        <Link className="item-wrapper" to={`/project/${item.id}`} key={item.id}>
+          <div className="portfolio-item-wrapper">
+            <img
+              className="thumbnail"
+              src={item.thumbUrl}
+              alt={`${item.name} Thumbnail`}
+            />
+            <img
+              className="logo"
+              src={item.logoUrl}
+              alt={`${item.name} Logo`}
+            />
 
             <div className="subtitle">{item.description}</div>
           </div>
@@ -27,8 +29,18 @@ const Homepage = () => {
 
   return (
     <div className="homepage-container">
-      {renderData(hobbies)}
-      {renderData(projects)}
+      <div className="filter-buttons-wrapper">
+        <button>Hobbies</button>
+
+        <button>Web Development</button>
+
+        <button>All</button>
+      </div>
+
+      <div className="data-wrapper">
+        {renderData(hobbies)}
+        {renderData(projects)}
+      </div>
     </div>
   );
 };
