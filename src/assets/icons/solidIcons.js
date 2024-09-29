f4f2b95082ec3a3abddeae612ae7ae2d20cba6a0
Andrewf9001 <andrewf9001@gmail.com>
2024-09-29 12:46:06 -0600

    added in contact information, added FA icons

--- src/assets/icons/solidIcons.js
+++ src/assets/icons/solidIcons.js
@@ -1,7 +1,14 @@
 import { library } from "@fortawesome/fontawesome-svg-core";
 
-import { faEdit, faSpinner, faTrash } from "@fortawesome/free-solid-svg-icons";
+import {
+  faEdit,
+  faEnvelope,
+  faMapMarkedAlt,
+  faPhone,
+  faSpinner,
+  faTrash,
+} from "@fortawesome/free-solid-svg-icons";
 
 export const solidIcons = () => {
-  library.add(faEdit, faTrash, faSpinner);
+  library.add(faEdit, faTrash, faSpinner, faEnvelope, faPhone, faMapMarkedAlt);
 };
--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,7 +1,26 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const Contact = () => {
   return (
     <div className="contact-container">
-      <h1>Contact</h1>
+      <div className="left-column"></div>
+
+      <div className="right-column">
+        <div className="row-wrapper">
+          <FontAwesomeIcon icon="fa-solid fa-phone" />
+          <div>888-888-8888</div>
+        </div>
+
+        <div className="row-wrapper">
+          <FontAwesomeIcon icon="fa-solid fa-envelope" />
+          <div>andrewf9001@gmail.com</div>
+        </div>
+
+        <div className="row-wrapper">
+          <FontAwesomeIcon icon="fa-solid fa-map-marked-alt" />
+          <div>SLC, UT</div>
+        </div>
+      </div>
     </div>
   );
 };
